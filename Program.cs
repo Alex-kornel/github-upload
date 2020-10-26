2e12555e08d701550c9451187c16e2c2ac0913fb
Alex <sashka.karatist@gmail.com>
2020-10-26 19:13:46 +0200

    initializing repository

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace lab_02a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            T1();
+            
+        }
+        public static void T1()
+        {
+            
+        }
+
+    }
+}
